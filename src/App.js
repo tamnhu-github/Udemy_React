98e66babc7aec9f28e681522b8dece9d9cf951ba
Trương Nguyễn Tâm Như <97598854+tamnhu-github@users.noreply.github.com>
2023-02-18 22:13:02 +0700

    video 16

--- src/App.js
+++ src/App.js
@@ -2,13 +2,15 @@ import logo from './logo.svg';
 import './App.css';
 import { useDispatch, useSelector } from 'react-redux';
 import { increaseCounter, decreaseCounter } from './redux/action/counterAction';
+import MyComponent from './components/MyComponent';
 const App = () => {
   const count = useSelector(state => state.counter.count);
   const dispatch = useDispatch();
 
   return (
     <div>
-      Hello World
+      Hello World với Eric &amp; Hỏi Dân IT
+      <MyComponent></MyComponent>
     </div>
     // <div className="App">
     //   <header className="App-header">
---
+++ src/components/MyComponent.js
@@ -0,0 +1,11 @@
+//class component
+//function component
+import React from "react";
+class MyComponent extends React.Component {
+    render() {
+        return (
+            <div>my first component</div>
+        );
+    }
+}
+export default MyComponent;
