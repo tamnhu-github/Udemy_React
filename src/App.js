8c01defbb523db6b321e458b827f1b54a9627dfa
Trương Nguyễn Tâm Như <97598854+tamnhu-github@users.noreply.github.com>
2023-02-24 23:37:50 +0700

    video48

--- src/App.js
+++ src/App.js
@@ -1,10 +1,23 @@
 import './App.scss';
 import Header from './components/Header/Header';
+import { Link } from 'react-router-dom';
 const App = () => {
 
   return (
     <div className="app-container">
       <Header/>
+      <div>
+        test hello
+        <div>
+          <button>
+            <Link to="/users">Go to user page</Link>
+            </button>
+          <button>
+            <Link to="/admin">Go to admin page</Link>
+          </button>
+
+        </div>
+      </div>
     </div>
   );
 }
