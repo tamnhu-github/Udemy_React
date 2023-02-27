bd3f25e7f62a0520111c8cc1836b3fe48aa0e81e
Trương Nguyễn Tâm Như <97598854+tamnhu-github@users.noreply.github.com>
2023-02-27 20:46:07 +0700

    video54

--- src/components/Admin/Admin.js
+++ src/components/Admin/Admin.js
@@ -1,6 +1,20 @@
+import { Sidebar } from "react-pro-sidebar";
+import SideBar from "./SideBar";
+import '../Admin/Admin.scss';
+import { FaBars } from 'react-icons/fa';
+import { useState } from "react";
 const Admin = (props) => {
+    const [collapsed, setCollapsed] = useState(false);
     return (
-        <div>admin component</div>
+        <div className="admin-container">
+            <div className="admin-sidebar">
+                <SideBar collapsed ={collapsed}/>
+            </div>
+            <div className="admin-content">
+                <FaBars style={{cursor: 'pointer'}} onClick={() => setCollapsed(!collapsed)}/>
+                Content goes here
+            </div>
+        </div>
     )
 }
 export default Admin;
--- src/index.js
+++ src/index.js
@@ -18,8 +18,8 @@ root.render(
         <Route path='/' element={<App/>}>        
           <Route index element={<Homepage/>} />
           <Route path='users' element={<User/>} />
-          <Route path='admin' element={<Admin/>} />
         </Route>
+        <Route path='admin' element={<Admin/>} />
       </Routes>
     </BrowserRouter>
     {/* </React.StrictMode> */}
