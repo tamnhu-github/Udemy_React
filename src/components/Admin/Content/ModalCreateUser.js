fda5c4769696d64bc047b3bb03e63868251c1b37
Trương Nguyễn Tâm Như <97598854+tamnhu-github@users.noreply.github.com>
2023-03-01 19:50:11 +0700

    videO60

--- src/components/Admin/Content/ModalCreateUser.js
+++ src/components/Admin/Content/ModalCreateUser.js
@@ -3,8 +3,8 @@ import React, { useState } from 'react';
 import Button from 'react-bootstrap/Button';
 import Modal from 'react-bootstrap/Modal';
 import {BsFillPlusCircleFill} from 'react-icons/bs';
-import axios from 'axios';
 import { toast } from 'react-toastify';
+import { postCreateNewUser } from '../../../services/apiService';
 
 const ModalCreateUser = (props) => {
   const { show, setShow } = props;
@@ -52,22 +52,13 @@ const ModalCreateUser = (props) => {
       return;
     }
     //call apis
-    
-    //data truyen len co file -> bat buoc dung formdata
-    const data = new FormData();
-    data.append('email', email);
-    data.append('password', password);
-    data.append('username', username);
-    data.append('role', role);
-    data.append('userImage', img);
-
-    let res = await axios.post('http://localhost:8081/api/v1/participant', data);
-    if(res.data && res.data.EC === 0) {
-      toast.success(res.data.EM);
+    let data = await postCreateNewUser(email, password, username, role, img);
+    if(data && data.EC === 0) {
+      toast.success(data.EM);
       handleClose();
     }
-    if(res.data && res.data.EC !== 0) {
-      toast.error(res.data.EM);
+    if(data && data.EC !== 0) {
+      toast.error(data.EM);
     }
   }
   return (
---
+++ src/services/apiService.js
@@ -0,0 +1,12 @@
+import axios from "../utils/axiosCustomize";
+const postCreateNewUser = (email, password, username, role, img) => {
+    //data truyen len co file -> bat buoc dung formdata
+    const data = new FormData();
+    data.append('email', email);
+    data.append('password', password);
+    data.append('username', username);
+    data.append('role', role);
+    data.append('userImage', img);
+    return axios.post('api/v1/participant', data);
+}
+export {postCreateNewUser};
